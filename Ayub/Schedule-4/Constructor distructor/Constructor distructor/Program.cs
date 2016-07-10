95f7a8c6b8d45b35b70a0550365464ac9d203a11
quadris <ayub.quadri89@gmail.com>
2016-07-11 00:57:24 +0530

    Schedule 4

---
+++ Ayub/Schedule-4/Constructor distructor/Constructor distructor/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public class A
+    {
+       
+        public int x;
+        public int y;
+        public int z;
+        
+        //Parameterized constructor
+        public A() { }
+
+        public A(int x)
+        {
+            this.x =x;
+        }
+        public A(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public A(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+
+        }
+       
+    }
+
+    public class X : A //accessing base class constructor with 2 parameters
+    {
+        public int c;
+        public X(int a, int b) : base(a, b)
+        {
+             c = x + y;
+        }
+    }
+
+    public class B
+    {
+        public string name;
+        public int age;
+        //default constructor
+        public B()
+        {
+            
+        }
+       
+    }
+
+    public class c
+    {
+        public bool IsInitialized;
+        private c ()
+        {
+            IsInitialized = true;
+        }
+    }
+
+    //public class P
+    //{
+    //    static readonly long baseline;
+
+    //    static P()
+    //    {
+    //        baseline = DateTime.Now.Ticks;
+    //        Console.WriteLine(baseline);
+    //    }
+    //}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Parameterized constructor
+            A p1 = new A(2);
+            Console.WriteLine("Single parameter, value of X ={0}", p1.x, p1.y, p1.z);
+
+            A obj = new A(5,6);
+            Console.WriteLine("Double parameter, value of X= {0} & Y= {1}", obj.x,obj.y);
+
+            A p3 = new A(14,15,16);
+            Console.WriteLine("Three parameter, value of X= {0} & Y= {1} & Z = {2}" , p3.x, p3.y,p3.z);
+
+            //accessing base class constructor from derived class
+            X result = new X(10,20);
+            Console.WriteLine("addition of {0} + {1} ={2}",result.x, result.y, result.c);
+
+            //default constructor
+            B person = new B();
+            Console.WriteLine("default constructor values of X= {0} & Y= {1}", person.name, person.age);
+            Console.ReadLine();
+
+            //Private constructor
+            //c init = new c(); // cannot instanciate private members.
+
+            //static constructor
+            //P statCont = new P();
+            
+        }
+    }
+}
---
+++ Ayub/Schedule-4/Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Opp
+    {
+        public int a;
+        public int b;
+        public virtual void operation(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+            Console.WriteLine("value of A={0} & B={1}",a,b);
+            
+        } 
+    }
+
+    class Add: Opp
+    {
+        public override void operation(int a, int b)
+        {
+            base.operation(a,b);
+            Console.WriteLine("Addition");
+            int c = a + b;
+            Console.WriteLine("{0} + {1} = {2}\n", a, b, c);
+            
+        }
+    }
+
+    class Sub : Opp
+    {
+        public override void operation(int a, int b)
+        {
+            base.operation(a,b);
+            Console.WriteLine("Substraction");
+            int c= a - b;
+            Console.WriteLine("{0} - {1} ={2}\n", a,b,c);
+            
+        }
+    }
+
+    class Mul : Opp
+    {
+        public sealed override void operation(int a, int b)
+        {
+            base.operation(a, b);
+            Console.WriteLine("Mulitplication");
+            int c = a * b;
+            Console.WriteLine("{0} * {1} ={2}\n", a, b, c);
+
+        }
+    }
+
+    //inheriting sealed class, uncomment this code u will get error.
+    //class X : Mul
+    //{
+    //    public override void operation(int a, int b) // cannot override inherited method bcoz its sealed.
+    //    {
+    //        base.operation(a, b);
+    //        Console.WriteLine("division");
+    //        int c = a / b ;
+    //        Console.WriteLine("{0} / {1} ={2} ", a, b, c);
+    //    }
+    //}
+
+    //access modiferes
+    class AccessModifiers
+    {
+        public int pub;
+        private int priv;
+        protected int pro;
+        internal int inter;
+
+        public void method()
+        {
+            pro = 3;
+            priv = 1;
+            pub = 9;
+            inter = 10;
+            Console.WriteLine("In base class so access to private = {0}  & Protected ={1} & internal ={2} & public ={3}\n",priv,pro,inter, pub);
+        }
+
+    }
+
+    class protectted : AccessModifiers
+    {
+        
+         public void method()
+         {
+             pro = 3;
+             inter = 1;
+             pub = 2;
+          //  Console.WriteLine("{0},{1}",priv,pro);  // in accessable due to protection level since private member 
+             Console.WriteLine("In derived class so only protected ={0} & internal ={1} & public ={2}\n", pro, inter, pub);
+         }
+        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Opp add = new Add();
+            add.operation(1, 5);
+
+            Opp sub = new Sub();
+            sub.operation(3, 2);
+
+            Opp mul = new Mul();
+            mul.operation(3, 2);
+
+            //Mul tripMul = new X();
+            //tripMul.operation(3, 6); // cannot override inherited method bcoz its sealed.
+
+            //access modifier
+            AccessModifiers acc = new AccessModifiers();
+            acc.pub = 2;
+          //acc.priv = 3; //inaccessable due to protection level
+          //acc.pro = 4; //inaccessable due to protection level
+            acc.inter = 5;
+
+            acc.method();
+
+            protectted derivved = new protectted();
+            derivved.method();
+
+            Console.WriteLine("accessable members from other classes are public = {0} & internal ={1}\n", acc.pub, acc.inter);
+
+            Console.ReadLine();
+        }
+    }
+}
